821c417bc09bcc66bb3dffec7ff1d834b02a2b18
HidDen-MaChinA <hei.franco.3@gmail.com>
2024-02-05 13:41:46 +0100

    feat: changed test file name

--- src/test/java/BuilderTests.java
+++ src/test/java/BuilderTests.java
@@ -5,8 +5,7 @@ import org.example.builder.Director;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-
-public class BuilderTest {
+public class BuilderTests {
     @Test
     public void createCarUsingDirector(){
         CarBuilder builder = new CarBuilder();
